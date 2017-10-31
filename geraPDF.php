43ca6df78350062c99ba569a972aa40c56c596bf
Alan Velasques Santos <alanvelasques@gmail.com>
2017-10-31 14:25:44 -0200

    teste de gravação no tj

--- geraPDF.php
+++ geraPDF.php
@@ -3,23 +3,13 @@
 
  $html = "
  <fieldset>
- <h1>Comprovante de Recibo</h1>
+ <h1>Teste</h1>
  <p class='center sub-titulo'>
- Nº <strong>0001</strong> -
- VALOR <strong>R$ 700,00</strong>
+ Nº <strong>908987987098098</strong> -
+ Tipo <strong>Conciliação</strong>
  </p>
- <p>Recebi(emos) de <strong>Ebrahim Paula Leite</strong></p>
- <p>a quantia de <strong>Setecentos Reais</strong></p>
- <p>Correspondente a <strong>Serviços prestados ..<strong></p>
- <p>e para clareza firmo(amos) o presente.</p>
- <p class='direita'>Itapeva, 11 de Julho de 2017</p>
- <p>Assinatura ......................................................................................................................................</p>
- <p>Nome <strong>Alberto Nascimento Junior</strong> CPF/CNPJ: <strong>222.222.222-02</strong></p>
- <p>Endereço <strong>Rua Doutor Pinheiro, 144 - Centro, Itapeva - São Paulo</strong></p>
+ <p>Olá</p>
  </fieldset>
- <div class='creditos'>
- <p><a href='https://www.webcreative.com.br/artigo/gerar-pdf-com-php-e-html-usando-a-biblioteca-mpdf' target='_blank'>Aprenda como gerar PDF com PHP e HTML usando a biblioteca MPDF aqui</a></p>
- </div>
  ";
 
  $mpdf=new mPDF();
@@ -27,6 +17,6 @@
  $css = file_get_contents("css/estilo.css");
  $mpdf->WriteHTML($css,1);
  $mpdf->WriteHTML($html);
- $mpdf->Output('../../../../moodledata_tj/filename.pdf','F');
+ $mpdf->Output('/sites/dev-conecta/PDF/pdf_tj_teste.pdf','F');
 
  exit;
