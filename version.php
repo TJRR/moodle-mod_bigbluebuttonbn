35b502b307df99fbe1871082a01dd4d5477c57c2
Luiz Henrique Longhi Rossi <lh.rossi@gmail.com>
2017-11-16 17:37:08 -0200

    alterando a version

--- version.php
+++ version.php
@@ -11,7 +11,7 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version  = 2017111600;
+$plugin->version  = 2017111601;
 $plugin->requires = 2013111800;
 $plugin->cron     = 0;
 $plugin->component = 'mod_bigbluebuttonbn';
